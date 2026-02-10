d1d27f063574d5c11593892b4ea66c215970c1fd
LZDQ <1486701401@qq.com>
2026-02-11 02:08:23 +0800

    Add back chain member textobj

--- lua/plugins/textobj.lua
+++ lua/plugins/textobj.lua
@@ -1,6 +1,6 @@
 return {
 	{ 'kana/vim-textobj-user',                      priority = 100 },
-	-- 'D4KU/vim-textobj-chainmember'  " m
+	{ 'D4KU/vim-textobj-chainmember',               event = 'VeryLazy' }, -- m
 	{ 'pianohacker/vim-textobj-indented-paragraph', event = 'VeryLazy' }, -- r, g(  and g) for jump
 	{ 'jceb/vim-textobj-uri',                       event = 'VeryLazy' }, -- iu for URL
 	{ 'Julian/vim-textobj-variable-segment',        event = 'VeryLazy' }, -- v
